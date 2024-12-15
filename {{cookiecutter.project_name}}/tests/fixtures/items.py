2ad6efec67774ed1318836ed7a6e4da3d805c1dc
Alex Ted <alextedx@gmail.com>
2024-12-15 10:16:26 +0300

    fix: returned the lost conditional statements (#5)

--- {{cookiecutter.project_name}}/tests/fixtures/items.py
+++ {{cookiecutter.project_name}}/tests/fixtures/items.py
@@ -1,9 +1,13 @@
+{% if cookiecutter.use_postgresql | lower == 'y' -%}
 import uuid
 from datetime import datetime
 from collections.abc import AsyncGenerator
+{% endif -%}
 
 import pytest
+{% if cookiecutter.use_postgresql | lower == 'y' -%}
 from tests.data import mock_data
+{% endif -%}
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
