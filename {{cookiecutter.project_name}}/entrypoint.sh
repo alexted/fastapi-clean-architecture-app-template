e39cd6febaf9ee2e2981676eea8d06cefb174329
Alex Ted <alextedx@gmail.com>
2025-01-04 18:48:04 +0300

    feat: entrypoints updated to use granian

--- {{cookiecutter.project_name}}/entrypoint.sh
+++ {{cookiecutter.project_name}}/entrypoint.sh
@@ -3,10 +3,10 @@
 [ -v ENVIRONMENT ] && export ENVIRONMENT=${ENVIRONMENT:-'LOCAL'}
 
 if [[ ${ENVIRONMENT} = 'LOCAL' ]]; then
-    exec gunicorn "src.service.application:create_app()" -n {{cookiecutter.project_name}} -k uvicorn.workers.UvicornWorker -b 0.0.0.0:5000 --log-level=debug
+    exec granian --interface asgi --process-name {{cookiecutter.project_name}} --host 0.0.0.0 --port 5000 --log-level debug --factory --reload "src.service.application:create_app()"
 else
     echo "Running database migrations..."
     alembic upgrade head
     echo "Starting application..."
-    exec gunicorn "src.service.application:create_app()" -n {{cookiecutter.project_name}} -k uvicorn.workers.UvicornWorker -w "${CONCURRENCY:-8}" -b 0.0.0.0:5000
+    exec granian --interface asgi --process-name {{cookiecutter.project_name}} --host 0.0.0.0 --port 5000  --workers "${CONCURRENCY:-8}" --factory "src.service.application:create_app"
 fi
