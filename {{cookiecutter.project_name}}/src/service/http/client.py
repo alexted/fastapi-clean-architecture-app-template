3ba1653d82e0fbbbf484404c75ca7c16bff3b300
Alex Ted <alextedx@gmail.com>
2025-02-08 20:28:36 +0300

    refactor: some improvements and optimizations for http client

--- {{cookiecutter.project_name}}/src/service/http/client.py
+++ {{cookiecutter.project_name}}/src/service/http/client.py
@@ -1,57 +1,27 @@
-import json as jsonlib
+import json
 import typing as t
 import logging
-from functools import reduce
+from functools import wraps
 
 import httpx
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
-Middleware = list[t.Callable[[t.Any, t.Callable], t.Any]] | None
-
-
-def strip_none(**kwargs: str) -> dict:
-    return {k: v for k, v in kwargs.items() if v is not None}
-
-
-def handler_factory(handler: t.Callable, middleware: t.Callable) -> t.Callable:
-    async def handle(data: dict) -> dict:
-        return await middleware(data, handler)
-
-    return handle
+Middleware = t.Optional[
+    list[t.Callable[[dict, t.Callable[..., t.Awaitable[httpx.Response]]], t.Awaitable[httpx.Response]]]
+]
 
 
 class HttpClient:
     def __init__(
-        self,
-        base_url: str,
-        query_params: dict = None,
-        headers: dict = None,
-        verify: bool = False,
-        **session_params: dict,
+        self, base_url: str, query_params: dict = None, headers: dict = None, verify: bool = False, **session_params
     ) -> None:
-        self._base_url: str = base_url
-        self._query_params: dict = query_params
-        self._headers: dict = headers
-        self._verify: bool = verify
-        self._session_params: dict = session_params
-        self.__session: httpx.AsyncClient | None = None
-
-    async def __aenter__(self) -> "HttpClient":
-        return self
-
-    async def __aexit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
-        await self.close()
-
-    async def close(self) -> None:
-        session, self.__session = self.__session, None
-        if session:
-            await session.aclose()
-
-    @property
-    def _session(self) -> httpx.AsyncClient:
-        if self.__session is None:
-            self.__session: httpx.AsyncClient = httpx.AsyncClient(
+        self._base_url = base_url.rstrip("/")
+        self._query_params = query_params or {}
+        self._headers = headers or {}
+        self._verify = verify
+        self._session_params = session_params
+        self._session: t.Optional[httpx.AsyncClient] = httpx.AsyncClient(
                 base_url=self._base_url,
                 params=self._query_params,
                 headers=self._headers,
@@ -59,91 +29,83 @@ class HttpClient:
                 **self._session_params,
             )
 
-        return self.__session
+    async def __aenter__(self) -> t.Self:
+        return self
 
-    async def call(  # noqa: CFQ002
+    async def __aexit__(self, *args) -> None:
+        await self.close()
+
+    async def close(self) -> None:
+        await self._session.aclose()
+
+    async def call(
         self,
         path: str = "",
         method: str = "GET",
-        data: dict = None,
-        json: dict = None,
-        params: dict = None,
-        headers: dict = None,
-        timeout: int = None,
-        files: bytes = None,
-        **kwargs: dict,
+        data: t.Optional[dict] = None,
+        json_data: t.Optional[t.Any] = None,
+        params: t.Optional[dict] = None,
+        headers: t.Optional[dict] = None,
+        timeout: t.Optional[int] = None,
+        files: t.Optional[bytes] = None,
+        **kwargs,
     ) -> httpx.Response:
-        """
-        Call the API method of the service. Repeats the interface of the request method of the httpx library
+        headers = {**self._headers, **(headers or {})}
+        if json_data is not None:
+            json_data = json.dumps(json_data, ensure_ascii=False) if not isinstance(json_data, str) else json_data
+            headers.setdefault("Content-Type", "application/json")
+            data = json_data
 
-        :param path:
-        :param method:
-        :param data:
-        :param json:
-        :param files:
-        :param params:
-        :param headers:
-        :param timeout:
-        :return: httpx.Response
-        """
-
-        if json is not None:
-            if isinstance(json, str):
-                data: str = json
-            else:
-                data: bytes = jsonlib.dumps(json, ensure_ascii=False).encode("utf-8")
-
-            if headers:
-                if "content-type" not in headers:
-                    headers["content-type"] = "application/json"
-            else:
-                headers = {"content-type": "application/json"}
-
+        url = f"{self._base_url}/{path.lstrip('/')}"
         logger.debug(
-            f"request: [{method}] {str(self._session.base_url) + path}\n"
-            f"params: {params}\n"
-            f"data: {data}\n"
-            f"headers: {headers}"
+            json.dumps(
+                {"method": method, "url": url, "params": params, "headers": headers, "data": data},
+                default=str,
+                indent=2,
+            )
         )
 
-        response: httpx.Response = await self._session.request(
-            url=path, method=method, data=data, headers=headers, params=params, timeout=timeout, files=files, **kwargs
+        response = await self._session.request(
+            method=method, url=url, data=data, headers=headers, params=params, timeout=timeout, files=files, **kwargs
         )
 
-        logger.debug(f"response: [{response.status_code}] {response.content}")
-
+        logger.debug(json.dumps({"status": response.status_code, "response": response.text}, default=str, indent=2))
+        response.raise_for_status()
         return response
 
 
 class ApiCall:
     def __init__(self, client: HttpClient, path: str = "", method: str = "GET", middleware: Middleware = None) -> None:
-        self._client: HttpClient = client
-        self._path: str = path
-        self._method: str = method
-        self._handler: t.Callable = reduce(handler_factory, (middleware or [])[::-1], self._call)
+        self._client = client
+        self._path = path
+        self._method = method
+        self._handler = self._compose_middleware(middleware)
+
+    def _compose_middleware(self, middleware: Middleware) -> t.Callable[[dict], t.Awaitable[httpx.Response]]:
+        handler = self._call
+        if middleware:
+            for mw in reversed(middleware):
+                handler = self._wrap_middleware(mw, handler)
+        return handler
+
+    @staticmethod
+    def _wrap_middleware(
+        middleware: t.Callable, handler: t.Callable[[dict], t.Awaitable[httpx.Response]]
+    ) -> t.Callable[[dict], t.Awaitable[httpx.Response]]:
+        @wraps(handler)
+        async def wrapped(parameters: dict) -> httpx.Response:
+            return await middleware(parameters, handler)
+
+        return wrapped
 
     async def _call(self, parameters: dict) -> httpx.Response:
-        path_params: dict = parameters.pop("path_params", {})
-        path: str = self._path.format(**path_params) if path_params else self._path
+        path_params = parameters.pop("path_params", {})
+        path = self._path.format(**path_params) if path_params else self._path
 
-        headers: dict = {**(self._client._session.headers or {}), **(parameters.get("headers") or {})}
-
-        if headers:
-            parameters["headers"] = headers
-
-        parameters.update({"method": self._method})
+        parameters["headers"] = {**self._client._headers, **parameters.get("headers", {})}
+        parameters["method"] = self._method
 
         return await self._client.call(path=path, **parameters)
 
-    async def __call__(  # noqa: CFQ002
-        self,
-        data: dict = None,
-        json: dict = None,
-        params: dict = None,
-        path_params: dict = None,
-        headers: dict = None,
-        **kwargs: dict,
-    ) -> t.Any:  # noqa ANN401
-        return await self._handler(
-            strip_none(data=data, json=json, params=params, path_params=path_params, headers=headers, **kwargs)
-        )
+    async def __call__(self, **kwargs) -> t.Any:
+        return await self._handler({k: v for k, v in kwargs.items() if v is not None})
