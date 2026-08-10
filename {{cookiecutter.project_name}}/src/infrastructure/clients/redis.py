75562a91ad37ff5e36eb0988281be1ea16582678
Alex Ted <alextedx@gmail.com>
2026-08-10 12:08:46 +0300

    build(deps): fix container images tags

--- {{cookiecutter.project_name}}/src/infrastructure/clients/redis.py
+++ {{cookiecutter.project_name}}/src/infrastructure/clients/redis.py
@@ -16,7 +16,7 @@ async def get_cache_client(config: Annotated[AppConfig, Depends(get_config)]) ->
 
     if _redis_client is None:
         _redis_client = Redis(
-            host=config.CACHE_DSN.host, port=config.CACHE_DSN.port, decode_responses=True, client_name=config.APP_NAME
+            host=config.REDIS_DSN.host, port=config.REDIS_DSN.port, decode_responses=True, client_name=config.APP_NAME
         )
 
     return _redis_client
